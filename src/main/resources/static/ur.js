123776fc5beae2c2f1aae227486e29d82bcbeb2b
natsuki-kining <natsuki_kining@126.com>
2020-08-09 10:22:31 +0800

    json key 转换

--- src/main/resources/static/ur.js
+++ src/main/resources/static/ur.js
@@ -1,20 +1,35 @@
 (function ($w, $vue, undefined) {
     const ur = {
         baseURL: 'http://localhost:8080/',
-        success: function (data, func) {
-            console.log(data);
+        success: function (response, func) {
+            console.log(response);
             if (func) {
-                func(data.data);
+                func(response.data);
             }
         },
-        error: function (data, func) {
-            console.log(data);
+        error: function (response, func) {
+            console.log(response);
             if (func) {
                 func(data);
             }
         },
+        /**
+         * 在传递给 then/catch 前，允许修改响应数据
+         * @param data
+         * @returns {any}
+         */
+        transformResponse:function(data){
+            let parse = JSON.parse(data);
+            ur.keyCase(parse);
+            return parse;
+        },
         ajax: function (config, successFunc, errorFunc) {
             config.url = config.url || this.baseURL + 'query';
+            if (!config.transformResponse){
+                if (config.useTransformResponse === undefined || config.useTransformResponse){
+                    config.transformResponse = ur.transformResponse;
+                }
+            }
             axios(config)
                 .then(function (data) {
                     ur.success(data, successFunc)
@@ -25,23 +40,62 @@
         },
         post: function (config, successFunc, errorFunc) {
             config = config || {};
-            config.method = 'post';
+            config.method = config.method || 'post';
             this.ajax(config, successFunc, errorFunc);
         },
         get: function (config, successFunc, errorFunc) {
             config = config || {};
-            config.method = 'get';
+            config.method = config.method || 'get';
             this.ajax(config, successFunc, errorFunc);
         },
-        query: function (queryCode, params, data, successFunc, errorFunc) {
-            data = data || {};
+        query: function (queryParam, params, successFunc, errorFunc) {
+            let data = {};
+            if (typeof queryParam === 'string') {
+                data.queryCode = queryParam;
+            } else {
+                data = queryParam;
+            }
             data.params = params || {};
-            data.queryCode = queryCode;
             let config = {
                 data: data
             };
             this.post(config, successFunc, errorFunc);
         },
+        /**
+         * 将map key转为驼峰格式
+         * @param json
+         */
+        keyCase: function (json) {
+            if (typeof json == 'object') {
+                if (Array.isArray(json)) {
+                    json.forEach(function (item) {
+                        ur.keyCase(item);
+                    })
+                } else {
+                    for (let key in json) {
+                        let item = json[key];
+                        if (typeof item == 'object') {
+                            ur.keyCase(item);
+                        }
+                        delete (json[key]);
+                        let newKey='';
+                        key = key.toLocaleLowerCase();
+                        for (let i = 0; i < key.length; i++) {
+                            let char = key.charAt(i);
+                            if (char == '_'){
+                                i++;
+                                char = key.charAt(i).toLocaleUpperCase();
+                                newKey+=char;
+                                continue;
+                            }
+                            newKey+=char;
+                        }
+                        json[newKey] = item;
+                    }
+                }
+            }
+
+        }
     }
     $w.ur = ur;
 })(window, Vue);
