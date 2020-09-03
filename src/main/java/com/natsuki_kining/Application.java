d093d660ff6730b95789f42476f454ee285a0bef
natsuki-kining <natsuki_kining@126.com>
2020-09-03 22:23:31 +0800

    引入ehcache

--- src/main/java/com/natsuki_kining/Application.java
+++ src/main/java/com/natsuki_kining/Application.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date : 2020/5/23 23:36
  */
 @SpringBootApplication
-@MapperScan(basePackages = {"com.natsuki_kining.ur.manager"})
+@MapperScan(basePackages = {"com.natsuki_kining.ur"})
 public class Application {
 
     public static void main(String[] args) {
