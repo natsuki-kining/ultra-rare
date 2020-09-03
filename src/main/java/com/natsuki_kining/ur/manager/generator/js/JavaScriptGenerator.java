7f00268c5ed318397c73e2de434f44f78722398e
natsuki-kining <natsuki_kining@126.com>
2020-09-03 22:00:14 +0800

    定义生成接口

---
+++ src/main/java/com/natsuki_kining/ur/manager/generator/js/JavaScriptGenerator.java
@@ -0,0 +1,15 @@
+package com.natsuki_kining.ur.manager.generator.js;
+
+import com.natsuki_kining.ur.manager.generator.AbstractGenerator;
+import com.natsuki_kining.ur.manager.generator.Generator;
+import org.springframework.stereotype.Component;
+
+/**
+ * TODO
+ *
+ * @Author : natsuki_kining
+ * @Date : 2020/9/3 21:53
+ */
+@Component
+public class JavaScriptGenerator extends AbstractGenerator implements Generator {
+}
